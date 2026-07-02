3a9568f749db101495c37debcc4d62ec350d967a
narxkim <narkim0215@gmail.com>
2026-07-02 16:19:52 +0900

    first commit

---
+++ day01-chat-client/src/main/java/study/day01_chat_client/Day01ChatClientApplication.java
@@ -0,0 +1,13 @@
+package study.day01_chat_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01ChatClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01ChatClientApplication.class, args);
+	}
+
+}
---
+++ day01-chat-client/src/test/java/study/day01_chat_client/Day01ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package study.day01_chat_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01ChatClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
